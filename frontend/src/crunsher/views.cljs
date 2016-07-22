9336b9a3fb14ce531cff91e8a963c1cec6b91208
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-07-22 10:32:25 +0200

    Update views

--- frontend/src/crunsher/views.cljs
+++ frontend/src/crunsher/views.cljs
@@ -9,21 +9,31 @@
   Object
   (render [this]
     (let [pokemon (om/props this)]
-      (dom/li nil (:name pokemon)))))
+      (dom/tr nil
+              (dom/td nil (:pokemon_id pokemon))
+              (dom/td nil (:name pokemon))
+              (dom/td nil (:individual_attack pokemon))
+              (dom/td nil (:individual_defense pokemon))
+              (dom/td nil (:individual_stamina pokemon))))))
 (def poketable-entry (om/factory PokeTableEntry {}))
 
 (defui PokeTable
   Object
   (render [this]
-    (dom/div nil
-             (apply dom/ul nil
-                    (map #(poketable-entry (lib/merge-react-key %)) (lib/inventory-pokemon))))))
+    (dom/table #js {:className "table table-hover"}
+               (dom/thead nil
+                          (dom/th nil "#")
+                          (dom/th nil "Name")
+                          (dom/th nil "Attack")
+                          (dom/th nil "Defense")
+                          (dom/th nil "Stamina"))
+               (apply dom/tbody nil
+                      (map #(poketable-entry (lib/merge-react-key %)) (lib/inventory-pokemon))))))
 (def poketable (om/factory PokeTable))
 
 (defui ^:once Main
   Object
   (render [this]
     (dom/div nil
-             (dom/h1 nil "Hello World"
-                     (dom/span #js {:className "small"} " Let's crunsh some Pokemon"))
+             (dom/h1 nil "Poké-Crunsher")
              (poketable))))
