7c1a8f8a4fb6c29e2f2190acbcd07dab7b8e740b
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-07-22 11:05:38 +0200

    Update views to use database

--- frontend/src/crunsher/views.cljs
+++ frontend/src/crunsher/views.cljs
@@ -3,18 +3,19 @@
             [om.dom :as dom :include-macros true]
             [goog.dom :as gdom]
             [crunsher.extensions]
-            [crunsher.lib :as lib]))
+            [crunsher.utils.lib :as lib]))
 
 (defui PokeTableEntry
   Object
   (render [this]
-    (let [pokemon (om/props this)]
+    (let [pokemon (om/props this)
+          pokemon-db (lib/get-pokemon-by-id (:pokemon-id pokemon))]
       (dom/tr nil
-              (dom/td nil (:pokemon_id pokemon))
-              (dom/td nil (:name pokemon))
-              (dom/td nil (:individual_attack pokemon))
-              (dom/td nil (:individual_defense pokemon))
-              (dom/td nil (:individual_stamina pokemon))))))
+              (dom/td nil (:pokemon-id pokemon))
+              (dom/td nil (:name pokemon-db))
+              (dom/td nil (:individual-attack pokemon))
+              (dom/td nil (:individual-defense pokemon))
+              (dom/td nil (:individual-stamina pokemon))))))
 (def poketable-entry (om/factory PokeTableEntry {}))
 
 (defui PokeTable
@@ -22,11 +23,12 @@
   (render [this]
     (dom/table #js {:className "table table-hover"}
                (dom/thead nil
-                          (dom/th nil "#")
-                          (dom/th nil "Name")
-                          (dom/th nil "Attack")
-                          (dom/th nil "Defense")
-                          (dom/th nil "Stamina"))
+                          (dom/tr nil
+                                  (dom/th nil "#")
+                                  (dom/th nil "Name")
+                                  (dom/th nil "Attack")
+                                  (dom/th nil "Defense")
+                                  (dom/th nil "Stamina")))
                (apply dom/tbody nil
                       (map #(poketable-entry (lib/merge-react-key %)) (lib/inventory-pokemon))))))
 (def poketable (om/factory PokeTable))
