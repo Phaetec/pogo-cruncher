2a9c097df3b29403b9bbf52b0af8293bff0227c0
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-07-22 14:54:40 +0200

    First om-next-devcards

--- frontend/src/devcards/crunsher/devcards/core.cljs
+++ frontend/src/devcards/crunsher/devcards/core.cljs
@@ -1,2 +1,12 @@
 (ns crunsher.devcards.core
-  (:require [crunsher.core]))
+  (:require [devcards.core :refer-macros [defcard deftest]]
+            [devcards-om-next.core :refer-macros [om-next-root defcard-om-next]]
+            [cljs.test :refer-macros [is testing]]
+            [crunsher.core]
+            [crunsher.utils.lib :as lib]
+            [crunsher.views :as views]))
+
+(defcard-om-next poketable
+                 "## Poketable"
+                 views/PokeTable
+                 lib/reconciler)
