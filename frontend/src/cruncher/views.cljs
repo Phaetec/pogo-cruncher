c229648b54ab3154db0b83f2dc733ad91c896e82
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-07-23 18:02:43 +0200

    Add props to vies

--- frontend/src/cruncher/views.cljs
+++ frontend/src/cruncher/views.cljs
@@ -64,7 +64,7 @@
     (dom/div nil
              (dom/div #js {:className "page-header"}
                       (dom/div #js {:className "pull-right"}
-                               (vlib/login-indicator))
+                               (vlib/login-indicator (om/props this)))
                       (dom/h1 nil "Poké-Cruncher"))
              (dom/p nil "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
              (dom/hr nil))))
@@ -158,7 +158,7 @@
   Object
   (render [this]
     (dom/div nil
-             (dom/div nil (header))
+             (dom/div nil (header (om/props this)))
              (dom/div nil (error-message (om/props this)))
              (view-dispatcher)
              #_(dom/div nil (poketable (om/props this)))
