dd0ca7afcb8efcf44fdabbae55e30a981af6c5e7
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-08-02 11:19:17 +0200

    Trigger progress bar

--- frontend/src/cruncher/shredder/main.cljs
+++ frontend/src/cruncher/shredder/main.cljs
@@ -4,23 +4,25 @@
             [cruncher.utils.lib :as lib]
             [cruncher.config :as config]
             [cruncher.communication.utils :as clib]
-            [cruncher.communication.main :as com]))
+            [cruncher.communication.main :as com]
+            [cruncher.communication.progress :as progress]))
 
 (defn success-handler
   "React on response after sending a new statement. Reset atom and call newly received url."
   [response]
-  (let [res (com/process-response response)]
-    (lib/info! "Converted Pokemon to bonbons. Jeah!")
-    (com/route :get-all-pokemon)))
+  (lib/update-progress-status! {:status "finished", :to_delete 1, :deleted 1})
+  (com/route :get-all-pokemon))
 
 (defn power-on
   "Crunch 'em all!"
-  []
+  [this]
   (let [url (:crunch-selected-pokemon config/api)
         selected-pokemon (vec (map #(.. % -value) (filter #(.. % -checked) (gdom/getElementsByClass "poketable-checkbox"))))]
     (when (pos? (count selected-pokemon))
       (when (js/confirm "Do you really want to send away the selected Pokemon?\nYou won't get them back!\n\nConsider your decision.")
         (lib/loading!)
+        (lib/update-progress-status! {:status "ok", :to_delete (count selected-pokemon), :deleted 0})
+        (progress/query-status this)
         (POST (clib/make-url url)
               {:body            (clib/clj->json {:ids selected-pokemon
                                                  :safe 1})
