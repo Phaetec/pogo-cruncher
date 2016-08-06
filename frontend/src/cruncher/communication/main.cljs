193d6b9e4c9858848654cd165fdc92a33b73cecc
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-08-06 17:11:40 +0200

    Add more error output if there are any errors

--- frontend/src/cruncher/communication/main.cljs
+++ frontend/src/cruncher/communication/main.cljs
@@ -5,6 +5,15 @@
             [cruncher.utils.lib :as lib]))
 
 ;;;; Generic helpers
+(defn something-went-wrong?
+  "Given a status message from the API, show an error message. Returns a boolean indicating errors."
+  [status message]
+  (if (= "error" status)
+    (do (lib/error! message)
+        true)
+    (do (lib/no-error!)
+        false)))
+
 (defn process-response
   "Generic success handler, which sets error handling and returns a cljs-compatible response."
   [response]
@@ -15,21 +24,30 @@
 ;;;; Handlers
 (defn error-handler
   "Generic error handler for ajax requests."
-  [{:keys [status status-text]}]
-  (lib/loading! false)
-  (.log js/console (str "Seems like you're not catching 'em all any more... " status " " status-text)))
+  [response]
+  (let [res (process-response response)
+        status-text (:status-text res)]
+    (if (= "Request failed" status-text)
+      (lib/error! "Could not reach our API. Maybe it is offline? Try to access http://localhost:5000")
+      (lib/error! (:message res)))
+    (.log js/console (str "Seems like you're not catching 'em all any more... " (:message res) status-text))))
 
 (defn success-handler
   "React on response after sending a new statement. Reset atom and call newly received url."
   [response]
-  (let [res (process-response response)]
-    (lib/update-pokemon! res)))
+  (let [res (process-response response)
+        errors? (something-went-wrong? (:status res) (:message res))]
+    (when-not errors?
+      (lib/update-pokemon! res))))
 
 (defn player-success-handler
   "React upon the receival of updated playerdata. Insert Information into app-state atom."
   [response]
-  (let [res (process-response response)]
-    (lib/update-player! res)))
+  (let [res (process-response response)
+        errors? (something-went-wrong? (:status res) (:message res))]
+    (when-not errors?
+      (lib/update-player! res))))
+
 
 ;;;; Requests
 (defn ajax-get
